738b225f07acf3164ca24094f7e6ddba56234a15
lingdocs <adueck@users.noreply.github.com>
2022-09-16 18:06:21 +0400

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,7 +96,7 @@ const Home: NextPage = () => {
         <samp>{submissionStatus}</samp>
       </div>
       <div className="text-center mt-4 text-muted">
-        <p className="lead">Made by <a className="em-link" href="https://lingdocs.com">LingDocs.com</a></p>
+        <p className="lead">Made by <a className="em-link" href="https://lingdocs.com">LingDocs</a></p>
         <p>Submissions are private. Nothing is kept on the server. See the <a className="em-link" href="https://github.com/lingdocs/rtl-epub-maker">source code here</a>.</p>
       </div>
       <FormatGuideModal show={showFormatGuide} onHide={() => setShowFormatGuide(false)} />
